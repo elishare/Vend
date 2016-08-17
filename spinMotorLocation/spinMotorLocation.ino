514402fbe8b7133a7e329b96a0c75329ef55beda
elishare <elishare@users.noreply.github.com>
2016-08-17 14:01:46 -0500

    Arduino pin mapping locations
    Define the pin names and locations for each motor control

--- spinMotorLocation/spinMotorLocation.ino
+++ spinMotorLocation/spinMotorLocation.ino
@@ -1,4 +1,42 @@
-int motorPin = 9;
+int motorPinA1 = 2;
+int motorPinA3 = 3;
+int motorPinA5 = 4;
+int motorPinA7 = 5;
+
+int motorPinB1 = 6;
+int motorPinB3 = 7;
+int motorPinB5 = 8;
+int motorPinB7 = 9;
+
+int motorPinC1 = 10;
+int motorPinC3 = 11;
+int motorPinC5 = 12;
+int motorPinC7 = 22;
+
+int motorPinD1 = 23;
+int motorPinD3 = 24;
+int motorPinD5 = 25;
+int motorPinD7 = 26;
+
+int motorPinE1 = 27;
+int motorPinE2 = 28;
+int motorPinE3 = 29;
+int motorPinE4 = 30;
+int motorPinE5 = 31;
+int motorPinE6 = 32;
+int motorPinE7 = 33;
+int motorPinE8 = 34;
+
+int motorPinF1 = 35;
+int motorPinF3 = 36;
+int motorPinF5 = 37;
+int motorPinF7 = 38;
+
+int motorPinG1 = 39;
+int motorPinG2 = 40;
+int motorPinG3 = 41;
+int motorPinG4 = 42;
+
 int LedPin = 13;
 char spot[4];
 int Letter, Number;
@@ -11,55 +49,61 @@ Serial.println("Live and working");
 }
 
 void loop() {
- while (Serial.available())  {
-  delay (30);
-  if (Serial.available()<5){
-     int Bytes = Serial.available();
+ while (Serial.available())  { //wait for serial connection to establish
+  delay (30); // delay for buffer to fill with all location information
+  if (Serial.available()<5){ //wait for serial buffer to have less than 5 bytes of information
+     int Bytes = Serial.available(); //assign Bytes to number of serial bytes available
      
-     for (int i=0; i<Bytes; i++)
-     {
-      spot[i] = Serial.read();
-      Serial.print(spot[i]);
-      //Serial.println(Bytes);
-        if (spot[i] == 'x'){
-          //Serial.println("moto");
+     for (int i=0; i<Bytes; i++) // with total bytes, assign an array with each byte in the serial command
+      {
+        spot[i] = Serial.read();
+        Serial.print(spot[i]); //debug print each time a byte is written to the array
+        //Serial.println(Bytes); //debug print number of Bytes
+          if (spot[i] == 'x'){ //look for termination byte 'x' to stop writing to array and jump into motor spin function
           
-          //int one = spot[0];
-          //int two = spot[1];
-          //int three = spot[2];
-          
-          //Serial.println(one);
-          //Serial.println(two);
-          //Serial.println(three);
-          
-          Letter = spot[0];
-          Number = spot[1];
-          
-          motor(Letter, Number); 
+            //Serial.println("moto"); //debug line to check if entered 'x' call
 
-      }
-     }
+            /* Debug commands to read array
+            int one = spot[0];
+            int two = spot[1];
+            int three = spot[2];
+          
+            Serial.println(one);
+            Serial.println(two);
+            Serial.println(three);
+            */
+          
+            Letter = spot[0];  //assign int Letter to location row in array
+            Number = spot[1];  //assign int Number to location column in array
+          
+           motor(Letter, Number); //call motor spin function. 
+
+          }
+       }
+    }
   }
-}
 
 }
 
 
 void motor(int row, int column){
-  //Serial.println("void");
-  //Serial.println(row);
-  char letter = row;
-  //Serial.println(letter);
-  char number = column;
-  if (letter == 'A' && number== '1'){
-  //Serial.println("location");
-  digitalWrite(motorPin, HIGH);
-  digitalWrite (LedPin, HIGH);
-  delay(10000); 
-  digitalWrite(motorPin, LOW);
-  digitalWrite (LedPin, LOW);
-  delay(10000);
-  flushMySerial();
+    //Serial.println("void");
+    //Serial.println(row);
+    char letter = row;
+    //Serial.println(letter);
+    char number = column;
+    if (letter == 'A' && number== '1'){
+    //Serial.println("location");
+    digitalWrite(motorPinA1, HIGH);
+    digitalWrite (LedPin, HIGH);
+    delay(2000); //spin motor for 2 seconds
+
+    //Stop motor spin
+    digitalWrite(motorPinA1, LOW);
+    digitalWrite (LedPin, LOW);
+    delay(100); //
+    
+    flushMySerial();
   }
   
   else
@@ -69,11 +113,10 @@ void motor(int row, int column){
     digitalWrite (LedPin, LOW);
     delay(1000);
     flushMySerial();
-}
+  }
 }
 
 void flushMySerial(){
   while(Serial.available())
   Serial.read();
 }
-
